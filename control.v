f104dd1ec7eba09567517a9c3fc90a0de31da3d7
Glauber da Silva Moura <silvamouraglauber@outlook.com>
2025-07-24 22:31:26 -0300

    add: control unit

---
+++ control.v
@@ -0,0 +1,93 @@
+module control (
+    PCSrc, ResultSrc, MemWrite, ALUControl, ALUSrc, ImmSrc, RegWrite,
+    op, funct3, funct7, Zero
+);
+    input       [6:0]   op;
+    input       [2:0]   funct3;
+    input               funct7;
+    input               Zero;
+
+    output              PCSrc;
+    output reg          ResultSrc;
+    output reg          MemWrite;
+    output reg  [2:0]   ALUControl;
+    output reg          ALUSrc;
+    output reg  [1:0]   ImmSrc;
+    output reg          RegWrite;
+
+    reg                 Branch;
+    reg         [1:0]   ALUOp;
+
+    assign PCSrc = Zero & Branch;
+
+    // Main Decoder
+    always @(*) begin
+        ResultSrc <= 0;
+        MemWrite <= 0;
+        ALUControl <= 3'b111; // Default ALU operation
+        ALUSrc <= 0;
+        ImmSrc <= 2'b00; // Default immediate source
+        RegWrite <= 0;
+        Branch <= 0;
+        ALUOp <= 2'b00; // Default ALU operation
+
+        case (op)
+            // LW
+            7'b000_0011: begin
+                RegWrite <= 1;
+                ALUSrc <= 1;
+                ResultSrc <= 1;
+                ALUOp <= 2'b00;
+            end
+            // SW
+            7'b010_0011: begin
+                MemWrite <= 1;
+                ALUSrc <= 1;
+                ImmSrc <= 2'b01;
+                ALUOp <= 2'b00;
+            end
+
+            // R-type
+            7'b011_0011: begin
+                RegWrite <= 1;
+                ALUOp <= 2'b10;
+            end
+
+            // BEQ
+            7'b110_0011: begin
+                ImmSrc <= 2'b11;
+                Branch <= 1;
+                ALUOp <= 2'b01;
+            end
+
+            default: begin
+                ResultSrc <= 0;
+                MemWrite <= 0;
+                ALUControl <= 3'b111;
+                ALUSrc <= 0;
+                ImmSrc <= 2'b00;
+                RegWrite <= 0;
+                Branch <= 0;
+                ALUOp <= 2'b00;
+            end
+        endcase
+    end
+
+    //ALU Decoder
+    always @(*) begin
+        case (ALUOp)
+            2'b00: ALUControl <= 3'b000; // ADD
+            2'b01: ALUControl <= 3'b001; // SUB
+            2'b10: begin
+                case (funct3)
+                    3'b000: ALUControl <= ({op[5], funct7} == 2'b11) ? 3'b001 : 3'b000; // ? SUB : ADD
+                    3'b010: ALUControl <= 3'b101; // SLT
+                    3'b110: ALUControl <= 3'b011; // OR
+                    3'b111: ALUControl <= 3'b010; // AND
+                    default: ALUControl <= 3'b111;
+                endcase
+            end
+            default: ALUControl <= 3'b111; // Default
+        endcase
+    end
+endmodule
---
+++ tb_control.v
@@ -0,0 +1,70 @@
+module tb_control;
+    reg     [6:0]   op;
+    reg     [2:0]   funct3;
+    reg             funct7;
+    reg             Zero;
+
+    wire            PCSrc;
+    wire            ResultSrc;
+    wire            MemWrite;
+    wire    [2:0]   ALUControl;
+    wire            ALUSrc;
+    wire    [1:0]   ImmSrc;
+    wire            RegWrite;
+
+    control dut(
+        .PCSrc(PCSrc), 
+        .ResultSrc(ResultSrc), 
+        .MemWrite(MemWrite), 
+        .ALUControl(ALUControl), 
+        .ALUSrc(ALUSrc), 
+        .ImmSrc(ImmSrc), 
+        .RegWrite(RegWrite),
+        .op(op), 
+        .funct3(funct3), 
+        .funct7(funct7), 
+        .Zero(Zero)
+    );
+
+    initial begin
+        op = 7'h0;
+        funct3 = 3'h0;
+        funct7 = 0;
+        Zero = 0;
+        #10;
+        op = 7'h03;
+        #10;
+        op = 7'h23;
+        #10;
+        op = 7'h63;
+        #10;
+        Zero = 1;
+        #10;
+        Zero = 0;
+        op = 7'h33;
+        #10;
+        {op[5], funct7} = {op[5], funct7} + 1'b1;
+        #10;
+        {op[5], funct7} = {op[5], funct7} + 1'b1;
+        #10;
+        {op[5], funct7} = {op[5], funct7} + 1'b1;
+        #10;
+        {op[5], funct7} = {op[5], funct7} + 1'b1;
+        #10;
+        funct3 = 3'h2;
+        #10;
+        funct3 = 3'h6;
+        #10;
+        funct3 = 3'h7;
+        #10;
+        funct3 = 3'h1;
+        #10;
+        funct3 = 3'h3;
+        #10;
+        funct3 = 3'h4;
+        #10;
+        funct3 = 3'h5;
+        #10;
+        funct3 = 3'h0;
+    end
+endmodule
