34b32d7c4e3984cbfef8e3ff727beb4ad330f56e
Glauber da Silva Moura <silvamouraglauber@outlook.com>
2025-07-26 10:22:12 -0300

    refactor: initializing memory with zeros and no write on R0

--- data_memory/data_memory.v
+++ data_memory/data_memory.v
@@ -7,14 +7,20 @@ module data_memory (
 );
 
     reg [31:0] mem [0:65535];
-
+    initial begin
+        integer i;
+        for (i = 0; i < 65536; i = i + 1) begin
+            mem[i] = 32'h00000000;
+        end
+    end
     always @(posedge CLK) begin
-        if(rst)
+        if(rst) begin
             mem[32'h2000] <= 10;
-        else
+        end else begin
             if (WE) begin
-                mem[A] <= WD;
+                if(A != 0) mem[A] <= WD;
             end
+        end
     end
 
     assign RD = mem[A];
