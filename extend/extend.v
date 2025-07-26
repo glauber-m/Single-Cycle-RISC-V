9bf9fe871fbb9cd3d908a924a6856e7694ec7132
Glauber da Silva Moura <silvamouraglauber@outlook.com>
2025-07-26 10:18:34 -0300

    refactor: extend

--- extend/extend.v
+++ extend/extend.v
@@ -1,43 +1,34 @@
 module extend(in, ImmSrc, out);
 
-input wire [24:0] in;
-input wire [1:0] ImmSrc;
-output reg [31:0] out;
+    input wire [24:0] in;
+    input wire [1:0] ImmSrc;
+    output reg [31:0] out;
 
-//ImmSrc --> 0 para load e 1 para store
-//ImmSrc --> 00: tipo I, 01: tipo S, 10 tipo B
-parameter [19:0] sinalExt = 20'b11111111111111111111;
+    //ImmSrc --> 0 para load e 1 para store
+    //ImmSrc --> 00: tipo I, 01: tipo S, 10 tipo B
 
-reg [31:0] aux;
+    wire [31:0] aux;
 
-assign aux[31:7] = in;
-assign aux[6:0] = 7'b0; //zero padding
+    assign aux[31:7] = in;
+    assign aux[6:0] = 7'b0; //zero padding
 
-always@(*)begin  
-
-    case (ImmSrc)
-        2'b00: begin
-                if(aux[31])out <= {sinalExt,aux[31:20]};
-                else out <= {8'b0,aux[31:20]};
+    always@(*)begin  
+        case (ImmSrc)
+            2'b00: begin
+                out <= {{20{aux[31]}},aux[31:20]};
             end
-        2'b01: begin
-                if(aux[31])out <= {sinalExt,aux[31:25],aux[11:7]};
-                else out <= {8'b0,aux[31:25],aux[11:7]};
-        end
-        2'b10: begin
-            if(aux[31])out <= {sinalExt,aux[7],aux[30:25],aux[11:8],1'b0};
-            else out <= {8'b0,aux[7],aux[30:25],aux[11:8],1'b0};
-        end
-        2'b11:begin
-            if(aux[31])out <= {sinalExt,aux[19:12],aux[20],aux[30:21],1'b0};
-            else out <= {8'b0,aux[19:12],aux[20],aux[30:21],1'b0};
-        end
-
-        default: out<=0;
-    endcase
-
+            2'b01: begin
+                out <= {{20{aux[31]}},aux[31:25],aux[11:7]};
+            end
+            2'b10: begin
+                out <= {{20{aux[31]}},aux[7],aux[30:25],aux[11:8],1'b0};
+            end
+            2'b11:begin
+                out <= {{12{aux[31]}},aux[19:12],aux[20],aux[30:21],1'b0};
+            end
+            default: out <= 32'd0;
+        endcase
     end
-
 endmodule
 
 /*
