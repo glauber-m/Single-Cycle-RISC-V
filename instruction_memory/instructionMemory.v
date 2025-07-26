221b1fd1d0735c0de942bdfe3cd4fc672d7f1910
César Godoy <cesarfsgodoy@gmail.com>
2025-07-26 20:23:45 -0300

    add: assembly do programa

--- instruction_memory/instructionMemory.v
+++ instruction_memory/instructionMemory.v
@@ -24,3 +24,29 @@ module instructionMemory (
         //else if(A > 32'h0000_0000_0001_0000 && A<32'h0000_0000_0001_00FF) RD = rom[(A >> 2)];
     end
 endmodule
+
+/*
+    pressupostos:
+    data_mem[0x2000] = 5
+    data_mem[0x2008] = 10
+    data_mem[0x200C] = 1
+    reg_file:
+        reg[0] = 0 (hardwired)
+        reg[9] = 0x2004 (inicialização)
+            
+
+
+    lw  x1, 8(x9)   // x1 ← mem[0x200C] = 1
+    lw  x2, -4(x9)  // x2 ← mem[0x2000] = 5
+    lw  x3, 4(x9)   // x3 ← mem[0x2008] = 10
+
+L1: sub x3, x3, x1  // x3 ← x3 - x1 (decrementa x3)
+    slt x4, x2, x3  // x4 ← (x2 < x3)? 1 : 0
+    beq x4, x0, L1  // se x2 >= x3, sai do loop
+
+    add x5, x2, x1      // x5 ← 5 + 1 = 6
+    and x6, x5, x0      // x6 ← 6 & 0 = 0
+    or  x6, x5, x0      // x6 ← 6 | 0 = 6
+    sw  x6, 0(x9)       // mem[0x2004] ← 6
+
+*/
