d30cf2e2eb95def16462a340880e9fa7e9bf03bb
danieldalarantes <danieldalarantes@gmail.com>
2025-07-31 11:33:52 -0300

    moving memory initialization to testbench #3 (instruction_memory.v)

---
+++ src/instruction_memory/instruction_memory.v
@@ -0,0 +1,12 @@
+module instruction_memory(
+    input  wire [31:0] A,  //  endereco
+    output wire [31:0] RD  //  dados
+);
+
+    //  declara a memoria rom
+    reg [31:0] rom [0:255];
+
+    //  le os dados do endereco selecionado
+    assign RD = rom[A >> 2];
+
+endmodule
