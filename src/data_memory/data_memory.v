211422e65e06c6fa3b6fac94ea744ea43825db3f
danieldalarantes <danieldalarantes@gmail.com>
2025-07-31 11:30:44 -0300

    moving memory initialization to testbench #1 (data_memory.v)

--- src/data_memory/data_memory.v
+++ src/data_memory/data_memory.v
@@ -1,4 +1,4 @@
-module data_memory (
+module data_memory(
     input  wire        CLK,
     input  wire        WE,
     input  wire [31:0] A,
@@ -14,13 +14,6 @@ module data_memory (
         if (WE) begin
             mem[A] <= WD;
         end
-    end
-
-    initial begin
-        // Initialize memory with some values for testing
-        mem[32'h2000] = 32'h00000005; // Example data at address 0x2000
-        mem[32'h2008] = 32'h0000000A; // Example data at address 0x2008
-        mem[32'h200C] = 32'h00000001; // Example data at address 0x200C
     end
 
 endmodule
