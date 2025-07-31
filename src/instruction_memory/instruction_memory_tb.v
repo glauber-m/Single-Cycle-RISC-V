b9490a7097d5c36aebe96bb262ae1d00f799c19d
danieldalarantes <danieldalarantes@gmail.com>
2025-07-31 11:34:45 -0300

    moving memory initialization to testbench #4 (instruction_memory_tb.v)

---
+++ src/instruction_memory/instruction_memory_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ns
+
+module instruction_memory_tb();
+
+    reg  [31:0] A;
+    wire [31:0] RD;
+
+    instruction_memory dut(A, RD);
+
+    // inicializa posicoes do instruction_memory com programa de teste
+    initial begin
+        $readmemh("/home/daniel/Single-Cycle-RISC-V-main/src/instruction_memory/test_1.txt", dut.rom);
+    end
+
+    initial begin
+        $display("Lendo dados de todos os enderecos da memoria...");
+
+        for(A = 0; A < 1024; A = A + 4) begin
+            #(10);
+            $display("Endereco: %3d | Dado: h%8h", (A >> 2), RD);
+        end
+
+        $finish;
+    end
+
+endmodule
